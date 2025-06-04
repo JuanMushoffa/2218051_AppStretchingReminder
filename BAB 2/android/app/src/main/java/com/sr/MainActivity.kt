9222e0d7e1d786c17b2edd5266a1afa7fd6b8248
JuanMushoffa <juannanda770@gmail.com>
2025-06-04 23:16:22 +0700

    Komponen dan Text Input

---
+++ BAB 2/android/app/src/main/java/com/sr/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.sr
+
+import com.facebook.react.ReactActivity
+import android.os.Bundle
+import com.facebook.react.ReactActivityDelegate
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
+import com.facebook.react.defaults.DefaultReactActivityDelegate
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView
+
+class MainActivity : ReactActivity() {
+
+  override fun getMainComponentName(): String = "SR"
+
+  override fun createReactActivityDelegate(): ReactActivityDelegate {
+    return object : DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled) {
+      override fun createRootView(): RNGestureHandlerEnabledRootView {
+        return RNGestureHandlerEnabledRootView(this@MainActivity)
+      }
+    }
+  }
+}
